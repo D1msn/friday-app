d195767abb7d3d8558eecf3082e2bcf656775259
D1msn <flash.volkov@gmail.com>
2022-02-04 19:39:10 +0300

    add hooks and more styles

--- src/containers/AuthContainer/AuthContainer.styled.ts
+++ src/containers/AuthContainer/AuthContainer.styled.ts
@@ -1,6 +1,12 @@
 import styled from 'styled-components'
 
 export const AuthContainerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  min-width: 413px;
+  min-height: 600px;
   background: #F9F9FE;
   border-radius: 8px;
 `
